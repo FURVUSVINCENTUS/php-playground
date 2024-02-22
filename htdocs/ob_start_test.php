47bc585cb7f7c6738acdf943a2d40e4e572ae9a3
Vicente Crestani <vicente.crestani@edu.vs.ch>
2024-02-22 17:07:06 +0100

    ob_start_test.php : tests des fonctionnalités des méthode apcu_...

--- htdocs/ob_start_test.php
+++ htdocs/ob_start_test.php
@@ -2,11 +2,20 @@
 if(isset($_POST['new_phrase'])){
   ob_start();
   echo (readfile('cache/phrases.html')."\n<li><a href='".$_POST['link']."'>".filter_input(INPUT_POST ,'new_phrase', FILTER_DEFAULT)."</a></li>");
-  $tampon = ob_get_contents();
+  //$tampon = ob_get_contents();
   //stockage du tampon dans une chaîne de chars
-  file_put_contents('cache/phrases.html', $tampon);
+  //file_put_contents('cache/phrases.html', $tampon);
+  file_put_contents('cache/phrases.html', ob_get_contents());
   ob_end_clean();
 }
+$code_id = 702;
+$tries = 10;
+
+$key = strval($tries);
+apcu_store(strval($code_id), $key);
+var_dump(apcu_fetch($code_id));
+print_r(apcu_cache_info());
+exit;
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
